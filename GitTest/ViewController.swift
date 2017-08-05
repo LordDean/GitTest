87ebe72c06da93ac0a8ae200e0a9b7a82401e873
LordDean <dean.mollica@gmail.com>
2017-08-05 17:45:15 +1000

    First working version of a test progress bar indicator

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var timer: Timer!
+    
+    var prog: Float = 0.0 {
+        didSet {
+            progressBar.setProgress(prog, animated: true)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        prog = 0.0
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        prog = 0.0
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(incrementProgress), userInfo: nil, repeats: true)
+        
     }
-
+    
+    func incrementProgress() {
+        
+        prog += 0.1
+        
+        if prog >= 1.0 {
+            timer.invalidate()
+        }
+        
+    }
 
 }
 
