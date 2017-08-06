f4afbd43db6560734f90044c6345807034f75692
LordDean <dean.mollica@gmail.com>
2017-08-06 13:13:40 +1000

    Cleaned up line spacing

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -23,35 +23,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         prog = 0.0
-        
     }
 
     @IBAction func buttonPressed(_ sender: Any) {
-        
         loadTimer?.invalidate()
         prog = 0.0
         activityIndicator.isHidden = false
         
         loadTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(incrementProgress), userInfo: nil, repeats: true)
-        
     }
     
     func incrementProgress() {
-        
         prog += 0.1
         
         if prog >= 1.0 {
             loadTimer.invalidate()
             disappearTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(hideLoading), userInfo: nil, repeats: false)
         }
-        
     }
     
     func hideLoading() {
-        
         UIView.animate(withDuration: 0.2, animations: { [unowned self] in
                 self.progressBar.alpha = CGFloat(0.0)
             }, completion: { [unowned self] _ in
